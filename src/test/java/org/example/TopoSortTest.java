e134d07f60cc223c7e3e9e9a75400f3e21c4845d
Sierra07-w <mukhammadsara07@gmail.com>
2025-11-03 00:15:36 +0500

    Update TopoSortTest.java

--- src/test/java/org/example/TopoSortTest.java
+++ src/test/java/org/example/TopoSortTest.java
@@ -1,4 +1,23 @@
 package org.example;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.*;
+
 public class TopoSortTest {
+
+    @Test
+    public void testTopoOrder() {
+        Map<Integer, List<int[]>> adj = new HashMap<>();
+        adj.put(0, List.of(new int[]{1, 1}));
+        adj.put(1, List.of(new int[]{2, 1}));
+        adj.put(2, new ArrayList<>());
+
+        KahnTopo topo = new KahnTopo();
+        List<Integer> order = topo.topoSort(adj);
+
+        assertEquals(3, order.size());
+        assertTrue(order.indexOf(0) < order.indexOf(1));
+        assertTrue(order.indexOf(1) < order.indexOf(2));
+    }
 }
